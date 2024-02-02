a756f6ffea480a3b62c5ee7a411f0cf0e1300514
novedice <Natalia.kitesurf@gmail.com>
2024-02-02 18:05:11 +0000

    feat: implement delete, list, rename functions

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { helperWithPath } from "./helperWithPathes.js";
+import fsPromises from "fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const thePath = await helperWithPath("files");
+    const files = await fsPromises.readdir(thePath);
+    // console.log(files);
+    for (let file of files) {
+      console.log(file);
+    }
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
