e1c4685c116e29859dfcf0b6ae7eeacdcf9e6390
novedice <natalia@Natalias-MacBook-Air.local>
2025-10-24 23:04:37 +0100

    add: implement streams functions

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
 const write = async () => {
-  // Write your code here
-};
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const pathToWriteFile = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+  const writeStream = fs.createWriteStream(pathToWriteFile);
+
+  process.stdin.pipe(writeStream);
+}
 
 await write();
