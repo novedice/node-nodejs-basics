9a68baf405fa7b0af57c6c55520b46f2ec9ce0d3
novedice <natalia@Natalias-MacBook-Air.local>
2025-10-25 13:19:54 +0100

    add: implement compress and decompress functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import zlib from 'node:zlib';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from "node:fs"
+
 const decompress = async () => {
-  // Write your code here
+
+  const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const passToArchive = path.join(__dirname,'files','archive.gz');
+    const passToFile = path.join(__dirname, 'files', 'fileToCompress.txt')
+
+    const readStream = fs.createReadStream(passToArchive);
+    const writeStream = fs.createWriteStream(passToFile);
+  
+    const decompress = zlib.createGunzip()
+  
+    readStream.pipe(decompress).pipe(writeStream);
 };
 
 await decompress();
