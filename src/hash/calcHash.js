c22992e6426590a93c4f482e807f71a0062962f4
novedice <natalia@Natalias-MacBook-Air.local>
2025-10-25 13:40:41 +0100

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import {fileURLToPath} from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs';
+import crypto from 'node:crypto';
+
 const calculateHash = async () => {
-  // Write your code here
+  
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const thePass = path.join(__dirname, 'files','fileToCalculateHashFor.txt');
+
+  const readStream = fs.createReadStream(thePass);
+  const hash = crypto.createHash('sha256');
+
+  const hashResult = readStream.pipe(hash);
+  console.log(hashResult.digest('hex'));
+
 };
 
 await calculateHash();
